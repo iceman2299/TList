2865e8779c173f95ff3d0ca3b3b1c2425df07033
iceman2299 <xxltri@gmail.com>
2023-12-06 09:54:21 +0300

    node, list

--- src/Tlist.cpp
+++ src/Tlist.cpp
@@ -1,1 +1,126 @@
-//файл заглушка
+#include <iostream>
+#include "Tlist.h"
+#include "Tlistnode.h"
+
+
+
+template <class T>
+TList<T>::TList(const TList<T>& p)
+{
+  count = p.count;
+  if (p.begin == nullptr)
+  {
+    begin = nullptr;
+    end = nullptr;
+  }
+  else
+  {
+    begin = new TListNode<T>(*p.begin);
+    TListNode<T>* temp = begin;
+    TListNode<T>* temp2 = p.begin;
+    while (temp2->GetNext() != p.begin)
+    {
+      TListNode<T>* d = new TListNode<T>();
+      d->SetPrev(temp);
+      d->SetData(new T(*temp2->GetNext()->GetData()));
+      temp->SetNext(d);
+      temp = temp->GetNext();
+      temp2 = temp2->GetNext();
+    }
+    end = new TListNode<T>(begin, temp, new T* (temp2->GetNext()->GetData()));
+    begin->SetPrev(end);
+  }
+}
+
+/*
+template <class T>
+TList<T>::TList()
+{
+  TListNode<T>* tmp;
+  int i = 0;
+  while (i < count)
+  {
+    TListNode<T>* tmp2 = tmp;
+    tmp = tmp->GetNext();
+    delete tmp2;
+    i++;
+  }
+  begin = nullptr;
+  end = nullptr;
+  count = 0;
+}
+*/ //ШТО ЕТА???
+template <class T>
+TList<T>::TList(TList<T>&& p)
+{
+  begin = p.begin;
+  end = p.end;
+  count - p.count;
+  p.begin = nullptr;
+  p.end = nullptr;
+  count = 0;
+}
+
+template <class T>
+TList<T>::~TList()
+{
+  begin = nullptr;
+  end = nullptr;
+  count = 0;
+}
+
+template<class T>
+TList<T>::TList(const TList<T>& p)
+{
+  count = p.count;
+}
+
+template <class T>
+void TList<T>::PushBegin(T* d)
+{
+  TListNode<T>* a = new TListNode(begin, end, d);
+  begin = a;
+}
+
+
+template <class T>
+void TList<T>::PushEnd(T* d)
+{
+  TListNode<T>* a = new TListNode(begin, end, d);
+  end = a;
+}
+
+template <class T>
+void TList<T>::DeleteBegin()
+{
+  TListNode<T>* a = begin;
+  end->SetNext(begin->GetNext());
+  begin = begin->GetNext();
+  begin->SetPrev(end);
+  delete a;
+}
+
+template <class T>
+void TList<T>::DeleteEnd()
+{
+  TListNode<T>* a = end;
+  begin->SetPrev(end->GetPrev());
+  end = end->GetPrev();
+  end->SetNext(begin);
+  delete a;
+}
+
+template <class T>
+TList<T>& TList<T>::operator=(const TList<T>&p)
+{
+  if (p == this)
+    return this;
+  TList<T>::TList(p);
+  return this;
+}
+
+template <class T>
+TListIterator<T> TList<T>::GetBegin()
+{
+  return TListIterator<T>(*this, begin);
+}
